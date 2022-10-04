c4e21ac7df9040487ef56d76670eec9b88976d31
水无垠 <2939428337@qq.com>
2022-10-04 12:02:17 +0800

    Update love.js

--- hhbirthday/css/love.js
+++ hhbirthday/css/love.js
@@ -1,10 +1,4 @@
 (function(window){
-<script>
-// 将以下代码添加到js入口函数内即可
-document.addEventListener('touchstart', function() {
-    document.getElementById('audios').play()
-})
-</script>
 	
 <script>
 			//音乐自动播放
@@ -583,47 +577,4 @@ document.addEventListener('touchstart', function() {
 
 })(window);
 
-<script>
-			//音乐自动播放
-			$(document).ready(function() {
-				autoPlayMusic();
-				audioAutoPlay();
-			});
-
-			function audioAutoPlay() {
-				var audio = document.getElementById('bg-music');
-				audio.play();
-				document.addEventListener("WeixinJSBridgeReady", function() {
-					audio.play();
-				}, false);
-			}
-			// 音乐播放
-			function autoPlayMusic() {
-				// 自动播放音乐效果，解决浏览器或者APP自动播放问题
-				function musicInBrowserHandler() {
-					musicPlay(true);
-					document.body.removeEventListener('touchstart', musicInBrowserHandler);
-				}
-				document.body.addEventListener('touchstart', musicInBrowserHandler);
-				// 自动播放音乐效果，解决微信自动播放问题
-				function musicInWeixinHandler() {
-					musicPlay(true);
-					document.addEventListener("WeixinJSBridgeReady", function() {
-						musicPlay(true);
-					}, false);
-					document.removeEventListener('DOMContentLoaded', musicInWeixinHandler);
-				}
-				document.addEventListener('DOMContentLoaded', musicInWeixinHandler);
-			}
-
-			function musicPlay(isPlay) {
-				var media = document.querySelector('#bg-music');
-				if (isPlay && media.paused) {
-					media.play();
-				}
-				if (!isPlay && !media.paused) {
-					media.pause();
-				}
-			}
-		</script>
 		
